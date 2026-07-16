7513bd0a5532e070a0680ae0ae56d887a9ad13fc
justfatlard <fatlard1993@gmail.com>
2026-07-16 14:35:13 -0700

    Restore shared components staged for deletion; the client migration replacing them is still in flight

---
+++ client/shared/GameInfoPopover.js
@@ -0,0 +1,32 @@
+import { Component, Button, styled } from '@vanilla-bean/components';
+
+import { getGame } from '../api';
+import GameInfoDialog from './GameInfoDialog.js';
+
+export default class GameInfoPopover extends (styled.Popover`
+	flex-direction: column;
+`) {
+	build() {
+		this._init();
+	}
+
+	async _init() {
+		const game = (await getGame(this.options.gameId)).body;
+
+		if (game.world?.name) {
+			new Component({ content: ` - ${game.world.name} - `, appendTo: this });
+		}
+		new Component({ content: `Game: ${game.name}`, appendTo: this });
+		new Component({ content: `Players: ${game.players.length}`, appendTo: this });
+		new Button({
+			content: 'More',
+			appendTo: this,
+			style: { display: 'block', margin: '6px auto' },
+			onPointerPress: () => {
+				this.elem.remove();
+
+				new GameInfoDialog({ game });
+			},
+		});
+	}
+}
